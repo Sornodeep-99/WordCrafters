ba5c5ce9d6a173d35566d7dcaf896bae87d9b984
Sornodeep-99 <100367731+TipuSultan23@users.noreply.github.com>
2023-07-31 13:28:39 +0530

    uploaded..

---
+++ public/js/firebase.js
@@ -0,0 +1,16 @@
+
+
+
+let firebaseConfig = {
+    apiKey: "AIzaSyANQB1r40ciPIuDFz7A8bQa5aL7PaqNpKo",
+    authDomain: "blogging-website-77f67.firebaseapp.com",
+    projectId: "blogging-website-77f67",
+    storageBucket: "blogging-website-77f67.appspot.com",
+    messagingSenderId: "401579305698",
+    appId: "1:401579305698:web:4e4dcf9e794720395c8cbb"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
